3a1dea21e9f11a37a9a5398a2c516edd7651ed49
Rishal <Rishal.mhd009@gmail.com>
2023-04-13 17:22:38 +0530

    main ui done, can add some features

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -3,11 +3,10 @@ import { API_KEY, imageUrl } from '../../constants/constants'
 import axios from '../../axios'
 import './Banner.css'
 function Banner() {
-    const random = Math.floor(Math.random() * 10) + 1;
     const [movie, setMovie] = useState()
     useEffect(()=> {
+          const random = Math.floor(Math.random() * 10) + 1;
           axios.get(`trending/all/week?api_key=${API_KEY}&language=en-US`).then((response)=>{
-            console.log(response.data.results[0])
             setMovie(response.data.results[random])
           })  
     }, [])
